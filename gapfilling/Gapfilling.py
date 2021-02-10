7a67ad962683571e87d5c3a0b7d63d347ad5cfe7
fcomnozz <fcomnozz@gmail.com>
2021-02-10 14:38:23 +0100

    New functions added; homology_gapfilling updated

--- gapfilling/Gapfilling.py
+++ gapfilling/Gapfilling.py
@@ -1,6 +1,6 @@
 import cobra
 import re
-from cobra.flux_analysis import gapfill
+from cobra.flux_analysis.gapfilling import GapFiller
 import gurobipy
 
 def load_query_model(model, obj = None):
@@ -31,7 +31,7 @@ def load_query_model(model, obj = None):
         try:
             model.objective = obj
         except ValueError:
-            print(str(obj) + "not found in the query model")
+            print(str(obj) + " not found in the query model")
             return model
         return model
     if obj == None:
@@ -74,8 +74,27 @@ def load_template_models(template_list, obj = None):
                 failures.append(str(model))
         print(failures)
         return templates
+    
+def gapfilling(model, template, integer_threshold, it = 1):
+    """
+    Calls GapFiller class from cobrapy.
+    """
+    gapfiller = GapFiller(model, template, integer_threshold = integer_threshold,
+                         demand_reactions = False)
+    return gapfiller.fill(iterations = it)
 
-def homology_gapfilling(model, templates, model_obj = None, template_obj = None):
+def add_exchange_reactions(model, template):
+    """
+    Adds all exchange reactions from a template matching model metabolites. 
+    """
+    EX = [template.reactions.get_by_id(i.id) for i in template.reactions if i.id.startswith("EX_")]
+    for reaction in EX:
+        if reaction not in model.reactions and str(list(reaction.metabolites.keys())[0]) in [i.id for i in model.metabolites]:
+            model.add_reaction(reaction.copy())
+    return model
+
+def homology_gapfilling(model, templates, model_obj = None, template_obj = None, use_all_templates = False,
+                       integer_threshold = 1e-6, force_exchange = False):
     """
     Performs gap filling on a model using homology models as templates.
     """
@@ -88,27 +107,48 @@ def homology_gapfilling(model, templates, model_obj = None, template_obj = None)
     value = model.optimize().objective_value
     if value == None:
         value = 0.0
-    for template in templates:
-        template.solver = 'gurobi'
-        # result variable will store the reactions ids
-        result = gapfill(model, template, demand_reactions = False)
-        # log
-        log = []
-        for reaction in result[0]:
-            if reaction.id.startswith("EX_"):
-                log.append((reaction.id, "Exchange reaction"))
-            else:
-                log.append((reaction.id, [str(list(reaction.genes)[i]) 
-                                          for i in range(len(list(reaction.genes)))]))
-        added_reactions[str(template)] = log
-        # Adding reactions to the model
-        [model.add_reaction(reaction.copy()) for reaction in result[0]]
-        # Flux will be evaluated here
-        new_value = model.optimize().objective_value
-        if new_value != None and new_value > value:
-            value = new_value
-        elif new_value == None:
-            continue
-        elif new_value != None and new_value == value:
-            break
-    return model, added_reactions
+    if use_all_templates == False:
+        for template in templates:
+            # adding exchange reactions
+            if force_exchange == True:
+                add_exchange_reactions(model, template)
+            template.solver = 'gurobi'
+            # result variable will store the reactions ids
+            result = gapfilling(model, template, integer_threshold = integer_threshold)
+            # reactions "log"
+            log = []
+            for reaction in result[0]:
+                if reaction.id.startswith("EX_"):
+                    log.append((reaction.id, "Exchange reaction"))
+                else:
+                    log.append((reaction.id, [str(list(reaction.genes)[i]) 
+                                              for i in range(len(list(reaction.genes)))]))
+            added_reactions[str(template)] = log
+            # Adding reactions to the model
+            [model.add_reaction(reaction.copy()) for reaction in result[0]]
+            # Flux will be evaluated here
+            new_value = model.optimize().objective_value
+            if new_value != None and new_value > value:
+                value = new_value
+            elif new_value == None:
+                continue
+            elif new_value != None and new_value == value:
+                break
+        return model, added_reactions
+    else:
+        for template in templates:
+            # adding exchange reactions
+            if force_exchange == True:
+                add_exchange_reactions(model, template)
+            template.solver = 'gurobi'
+            result = gapfilling(model, template, integer_threshold = integer_threshold)
+            log = []
+            for reaction in result[0]:
+                if reaction.id.startswith("EX_"):
+                    log.append((reaction.id, "Exchange reaction"))
+                else:
+                    log.append((reaction.id, [str(list(reaction.genes)[i]) 
+                                              for i in range(len(list(reaction.genes)))]))
+            added_reactions[str(template)] = log
+            [model.add_reaction(reaction.copy()) for reaction in result[0]]
+        return model, added_reaction
